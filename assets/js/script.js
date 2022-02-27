e6274fc267500971bd092ea3fe2f386733e0e326
alanFMA <alanmonteiro105@gmail.com>
2022-02-27 03:13:53 -0300

    Máscara com JS Puro

---
+++ assets/js/script.js
@@ -0,0 +1,20 @@
+function chamar() {
+   let qtd = document.getElementById('inputNumber').value;
+   let vlr = document.getElementById('valor').value;
+   let res = qtd * vlr;
+    vlr.innerHTML = `${res}`;
+}
+
+function mascara(i){
+   
+    var v = i.value;
+    
+    if(isNaN(v[v.length-1])){ // impede entrar outro caractere que não seja número
+       i.value = v.substring(0, v.length-1);
+       return;
+    }
+    
+    i.setAttribute("maxlength", "14");
+    if (v.length == 3 || v.length == 7) i.value += ".";
+    if (v.length == 11) i.value += "-";
+ }
