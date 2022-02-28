a9cdd5de317b23ca9a43b712473334a29e90d23e
alanFMA <alanmonteiro105@gmail.com>
2022-02-28 12:32:52 -0300

    Ajuste de validação

--- assets/js/script.js
+++ assets/js/script.js
@@ -20,31 +20,18 @@ function pegaNumber() {
      if (v.length == 11) i.value += "-";
   }
 
-  document.querySelector('input[type=email]').oninvalid = function() {
+  document.querySelector('input#cpf').oninvalid = function() {
 
-   // remove mensagens de erro antigas
-   this.setCustomValidity("");
- 
-   // reexecuta validação
-   if (!this.validity.valid) {
- 
-       // se inválido, coloca mensagem de erro
-       this.setCustomValidity("Email obrigatório");
-   }
- };
+    // remove mensagens de erro antigas
+    this.setCustomValidity("");
+  
+    // reexecuta validação
+    if (!this.validity.valid) {
+  
+        // se inválido, coloca mensagem de erro
+        this.setCustomValidity("CPF inválido");
+    }
+  };
 
-  document.querySelector('input#cvv').oninvalid = function() {
 
-   // remove mensagens de erro antigas
-   this.setCustomValidity("");
- 
-   // reexecuta validação
-   if (!this.validity.valid) {
- 
-       // se inválido, coloca mensagem de erro
-       this.setCustomValidity("Padrão inválido para CVV");
-       
-   }
- };
 
- 
