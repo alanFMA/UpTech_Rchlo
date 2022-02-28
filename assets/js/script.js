f41ecb419345c45f057774b3e0ec90d21f28354c
alanFMA <alanmonteiro105@gmail.com>
2022-02-28 01:03:20 -0300

    Validação HTML5 e calculo do total JS

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,20 +1,50 @@
-function chamar() {
-   let qtd = document.getElementById('inputNumber').value;
-   let vlr = document.getElementById('valor').value;
-   let res = qtd * vlr;
-    vlr.innerHTML = `${res}`;
-}
+var numero = 69.90;
 
-function mascara(i){
-   
-    var v = i.value;
-    
-    if(isNaN(v[v.length-1])){ // impede entrar outro caractere que não seja número
-       i.value = v.substring(0, v.length-1);
-       return;
-    }
-    
-    i.setAttribute("maxlength", "14");
-    if (v.length == 3 || v.length == 7) i.value += ".";
-    if (v.length == 11) i.value += "-";
+function pegaNumber() {
+   var qtd = document.getElementById('input-number').value
+   var res = qtd * numero;
+   document.getElementById('valor').value = res.toLocaleString("pt-BR", { minimumFractionDigits: 2, style:'currency', currency: 'BRL'});;
  }
+ 
+ function mascara(i){
+   
+     var v = i.value;
+    
+     if(isNaN(v[v.length-1])){ // impede entrar outro caractere que não seja número
+        i.value = v.substring(0, v.length-1);
+        return;
+     }
+    
+     i.setAttribute("maxlength", "14");
+     if (v.length == 3 || v.length == 7) i.value += ".";
+     if (v.length == 11) i.value += "-";
+  }
+
+  document.querySelector('input[type=email]').oninvalid = function() {
+
+   // remove mensagens de erro antigas
+   this.setCustomValidity("");
+ 
+   // reexecuta validação
+   if (!this.validity.valid) {
+ 
+       // se inválido, coloca mensagem de erro
+       this.setCustomValidity("Email obrigatório");
+   }
+ };
+
+  document.querySelector('input#cvv').oninvalid = function() {
+
+   // remove mensagens de erro antigas
+   this.setCustomValidity("");
+ 
+   // reexecuta validação
+   if (!this.validity.valid) {
+ 
+       // se inválido, coloca mensagem de erro
+       this.setCustomValidity("Padrão inválido para CVV");
+       
+   }
+ };
+
+ 
