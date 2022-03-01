08a6a601ede2a79e549e05fa9268da6ac2b12dea
alanFMA <alanmonteiro105@gmail.com>
2022-02-28 21:42:15 -0300

    conclusão

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,7 +6,7 @@ function pegaNumber() {
    document.getElementById('valor').value = res.toLocaleString("pt-BR", { minimumFractionDigits: 2, style:'currency', currency: 'BRL'});;
  }
  
- function mascara(i){
+ function mascaracpf(i){
    
      var v = i.value;
     
@@ -20,18 +20,50 @@ function pegaNumber() {
      if (v.length == 11) i.value += "-";
   }
 
-  document.querySelector('input#cpf').oninvalid = function() {
+//   document.getElementById('cpf').oninvalid = function() {
 
-    // remove mensagens de erro antigas
-    this.setCustomValidity("");
+//     // remove mensagens de erro antigas
+//     this.setCustomValidity("");
   
-    // reexecuta validação
-    if (!this.validity.valid) {
+//     // reexecuta validação
+//     if (!this.validity.valid) {
   
-        // se inválido, coloca mensagem de erro
-        this.setCustomValidity("CPF inválido");
-    }
-  };
+//         // se inválido, coloca mensagem de erro
+//         this.setCustomValidity("CPF inválido");
+//     }
+//   };
+
+function mascaracartao(o,f){
+v_obj=o
+v_fun=f
+setTimeout("execmascara()",1)
+}
+function execmascara(){
+   v_obj.value=v_fun(v_obj.value)
+}
+function mcc(v){
+   v=v.replace(/\D/g,"");
+   v=v.replace(/^(\d{4})(\d)/g,"$1 $2");
+   v=v.replace(/^(\d{4})\s(\d{4})(\d)/g,"$1 $2 $3");
+   v=v.replace(/^(\d{4})\s(\d{4})\s(\d{4})(\d)/g,"$1 $2 $3 $4");
+   return v;
+}
+function id(cc){
+  return document.getElementById(cc);
+}
+window.onload = function(){
+  id('cc').onchange = function(){
+     mascaracartao(this, mcc);
+  }
+}
 
 
-
+function onValidate(el, messageError) {
+   el.setCustomValidity("");
+   el.style.boxShadow = null;
+   
+   if (!el.validity.valid) {
+      el.setCustomValidity(messageError);
+      el.style.boxShadow = "0 0.5px 0.5px rgba(0,0,0, 0.075) inset, 0 0 5px #c22828";
+   }
+}
